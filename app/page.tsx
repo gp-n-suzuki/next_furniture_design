5df9e2064f171a97d8e5bcbeada3ad2b1319eebb
gp-n-suzuki <nobuaki_s@gp1000.jp>
2024-09-26 12:53:09 +0900

    トップページ実装

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,22 @@
-export default function Home() {
+import { Item } from "@/types/Item";
+import styles from "./page.module.css"
+import Link from "next/link";
+import Card from "./_components/Card/Card";
+
+export default async function Home() {
+
+  const limit = 8;
+  const res = await fetch(`http://localhost:3000/api?limit=${limit}`, {cache: 'no-store'})
+  const {items}:{items:Item[]} = await res.json();
+  
   return (
-    <div>
+    <div className={styles.wrapper}>
+      <div className={styles.grid_container}>
+        <Card items={items} />
+      </div>
+      <div className={styles.more}>
+        <Link href={"/products/1"}>View More</Link>
+      </div>
     </div>
   );
 }
